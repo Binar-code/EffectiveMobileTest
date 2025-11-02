fbb9eeb27e38892dc7a61cdc885910bad76e9234
Binar <yardemyd@gmail.com>
2025-11-02 19:03:55 +0300

    Style fixes

--- app/src/main/java/com/example/effectivemobiletest/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/effectivemobiletest/ui/account/AccountFragment.kt
@@ -1,10 +1,6 @@
 package com.example.effectivemobiletest.ui.account
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.effectivemobiletest.R
+
 class AccountFragment : Fragment() {
 }
